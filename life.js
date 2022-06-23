81414c04726fccaf2a958b900a710a2164d847e2
Christiana Eke <105323627+Khryztie@users.noreply.github.com>
2022-06-23 20:08:06 +0000

    Update life.js

--- life.js
+++ life.js
@@ -1,3 +1,4 @@
 alert(`My name is Christiana Eke.
 I am 5.5ft. tall.
-A citizen of Nigeria`);
+A citizen of Nigeria`)
+document.write('<a href="https://github.com/Khryztie/JS_profile"> Go to github repo</a>');
